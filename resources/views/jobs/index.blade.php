410a3dca81b93a8b59c9cd6ad3f51a100d74abd4
Adil Waheed <cy4.awd@gmail.com>
2025-04-24 18:34:07 +0500

    Desigining Template Inheritance

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,19 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jobs Listings</title>
-</head>
-    <body>
-        <h1>Avaiable jobs</h1>
-        <ul>
-            @forelse($jobs as $job)
-                
-                <li>{{$job}}</li>
-            @empty
-                <p>No Jobs avaiable</p>
-            @endforelse
-        </ul>
-    </body>
-</html>
+@extends('layout')
+
+@section('content')
+    <h1>Available jobs</h1>
+    <ul>
+        @forelse($jobs as $job)
+            
+            <li>{{$job}}</li>
+        @empty
+            <p>No Jobs avaiable</p>
+        @endforelse
+    </ul>
+@endsection
+
---
+++ resources/views/jobs/show.blade.php
@@ -0,0 +1,4 @@
+@extends('layout')
+@section('content')
+    <h1>Show Job {{$id}}</h1>
+@endsection
