bca19d389ad13051394ea6661deae1a561871038
Adil Waheed <cy4.awd@gmail.com>
2025-06-26 06:02:53 +0500

    tailwindcss installed and header component update

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -3,8 +3,8 @@
     <h1>Create New Job</h1>
     <form action="/jobs" method="POST">
         @csrf
-        <input type="text" name="title" placeholder="title">
-        <input type="text" name="description" placeholder="description">
+        <input class="border-4 border-indigo-500 rounded-md" type="text" name="title" placeholder="title">
+        <input class="border-4 border-indigo-500 rounded-md" type="text" name="description" placeholder="description">
         <button type="submit">Submit</button>
     </form>
 </x-layout>
